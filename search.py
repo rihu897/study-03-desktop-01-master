a2ba971c33652c7a067fbb0e62864b726a38ec57
rihu897 <73801985+rihu897@users.noreply.github.com>
2020-11-16 21:52:08 +0900

    Add files via upload

--- search.py
+++ search.py
@@ -4,26 +4,30 @@ import eel
 ### デスクトップアプリ作成課題
 ### 課題6：CSV保存先をHTML画面から指定可能にする
 def kimetsu_search(word, csv):
-    # 検索対象取得
-    df=pd.read_csv("./" + csv)
-    source=list(df["name"])
-    # 検索結果
-    result = ""
-    # 検索
-    if word in source:
-        result = "『{}』はあります".format(word)
-    else:
-        result = "『{}』はありません".format(word)
-        # 追加
-        #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
-        #if add_flg=="1":
-        source.append(word)
-    
-    # 検索結果をコンソールに出力
-    print(result)
-    # CSV書き込み
-    df=pd.DataFrame(source,columns=["name"])
-    df.to_csv("./" + csv,encoding="utf_8-sig")
-    print(source)
+    # トランザクション開始
+    try :
+        # 検索対象取得
+        df=pd.read_csv("./" + csv)
+        source=list(df["name"])
+        # 検索結果
+        result = ""
+        # 検索
+        if word in source:
+            result = "『{}』はあります".format(word)
+        else:
+            result = "『{}』はありません".format(word)
+            # 追加
+            #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
+            #if add_flg=="1":
+            source.append(word)
+        
+        # 検索結果をコンソールに出力
+        print(result)
+        # CSV書き込み
+        df=pd.DataFrame(source,columns=["name"])
+        df.to_csv("./" + csv,encoding="utf_8-sig")
+        print(source)   
+    except :
+        result = "ERROR:CSVファイルが見つかりません"
     # 検索結果を返却
     return result
